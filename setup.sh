e62cc8caf2c841bfcd6d4963ac1337be79c6afa9
James Price <jimmyjayp@gmail.com>
2019-10-21 16:47:48 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 export PATH=$PATH:/Users/jj/src/regression-testing/bin
-export CSRT_TEST_NAME=jj+test2
-export CSRT_API=pd-api.codestream.us:9443
+export CSRT_TEST_NAME=jj+test6
+export CSRT_API=api.codestream.com
+#export CSRT_API=jj1.codestream.us
+#export CSRT_API=pd-api.codestream.us:9443
+#export CSRT_API=qa-api.codestream.us
