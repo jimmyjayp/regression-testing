95a308b47bde4ab5218355756898b01c3a2ff8d3
James Price <jimmyjayp@gmail.com>
2019-10-22 15:38:13 -0400

    using regression testing from jimmyjayp github user

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export PATH=$PATH:/Users/jj/src/regression-testing/bin
-export CSRT_TEST_NAME=jj+test6
-export CSRT_API=api.codestream.com
+export CSRT_TEST_NAME=jj+test1
+#export CSRT_API=api.codestream.com
 #export CSRT_API=jj1.codestream.us
 #export CSRT_API=pd-api.codestream.us:9443
 #export CSRT_API=qa-api.codestream.us
