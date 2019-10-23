869e1d0e6ab59618748f2bef1ad189c141de948f
James Price <jimmyjayp@gmail.com>
2019-10-23 17:20:29 -0400

    set api yourself

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ export CSRT_TEST_NAME=jj+test1
 #export CSRT_API=jj1.codestream.us
 #export CSRT_API=pd-api.codestream.us:9443
 #export CSRT_API=qa-api.codestream.us
+#export CSRT_API=abc-api.codestream.us
+echo "Make sure you set 'export CSRT_API='"
