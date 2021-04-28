73583bcbe89a6ec68e397bee4c65e3ac2bd641d9
James Price <jimmyjayp@gmail.com>
2021-04-28 13:18:07 -0400

    Bunch o' stuff

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 export PATH=$PATH:/Users/jj/src/regression-testing/bin
-export CSRT_TEST_NAME=jj+test1
+#export CSRT_TEST_NAME=jj+test1
 #export CSRT_API=api.codestream.com
 #export CSRT_API=jj1.codestream.us
 #export CSRT_API=pd-api.codestream.us:9443
 #export CSRT_API=qa-api.codestream.us
 #export CSRT_API=abc-api.codestream.us
-echo "Make sure you set 'export CSRT_API='"
