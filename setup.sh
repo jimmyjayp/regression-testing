068a9f54e217f9fed4d03493a1ac614dc6531401
James Price <jimmyjayp@gmail.com>
2021-07-12 19:39:13 -0400

    after installing on new notebook

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-export PATH=$PATH:/Users/jj/src/regression-testing/bin
+export PATH=$PATH:$HOME/src/regression-testing/bin
 #export CSRT_TEST_NAME=jj+test1
 #export CSRT_API=api.codestream.com
 #export CSRT_API=jj1.codestream.us
 #export CSRT_API=pd-api.codestream.us:9443
 #export CSRT_API=qa-api.codestream.us
 #export CSRT_API=abc-api.codestream.us
+
+echo "
+To setup a new test...
+
+rt-setup -t test1 -i demo-api
+"
